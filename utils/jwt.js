1f27dcddde1c7176e93dbddb722d2cb53d7a26c4
ixahil <sahilshaiikh@hotmail.com>
2023-10-13 16:28:54 +0530

    domain localhost added

--- utils/jwt.js
+++ utils/jwt.js
@@ -12,13 +12,15 @@ export const accessTokenOptions = {
   expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
   maxAge: accessTokenExpire * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "None",
+  sameSite: "none",
+  domain: "localhost",
 };
 export const refreshTokenOptions = {
   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "None",
+  sameSite: "none",
+  domain: "localhost",
 };
 
 export const sendToken = (user, statusCode, res) => {

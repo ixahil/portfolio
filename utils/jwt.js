696800297220bbfe8067bcc5e8f0d1661582a27f
ixahil <sahilshaiikh@hotmail.com>
2023-10-13 16:59:40 +0530

    change

--- utils/jwt.js
+++ utils/jwt.js
@@ -13,12 +13,14 @@ export const accessTokenOptions = {
   maxAge: accessTokenExpire * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "None",
+  domain: "github.com",
 };
 export const refreshTokenOptions = {
   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "None",
+  domain: "github.com",
 };
 
 export const sendToken = (user, statusCode, res) => {

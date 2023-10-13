1b337c60d5a37d827504a08afab830ee5ae5f64d
ixahil <sahilshaiikh@hotmail.com>
2023-10-13 16:52:35 +0530

    rolback

--- utils/jwt.js
+++ utils/jwt.js
@@ -13,16 +13,12 @@ export const accessTokenOptions = {
   maxAge: accessTokenExpire * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "None",
-  Domain: "localhost",
-  secure: true,
 };
 export const refreshTokenOptions = {
   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
   httpOnly: true,
   sameSite: "None",
-  domain: "localhost",
-  secure: true,
 };
 
 export const sendToken = (user, statusCode, res) => {
@@ -33,6 +29,10 @@ export const sendToken = (user, statusCode, res) => {
   redis.set(user._id, JSON.stringify(user));
 
   // Only set secure to true in production
+  if (process.env.NODE_ENV === "production") {
+    accessTokenOptions.secure = true;
+    refreshTokenOptions.secure = true;
+  }
 
   res.cookie("access_token", accessToken, accessTokenOptions);
   res.cookie("refresh_token", refreshToken, refreshTokenOptions);

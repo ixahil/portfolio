92cc5e9b1253cec69529d3c436511ed7509b9624
ixahil <sahilshaiikh@hotmail.com>
2023-10-16 21:16:53 +0530

    testing

--- utils/jwt.js
+++ utils/jwt.js
@@ -12,13 +12,13 @@ export const accessTokenOptions = {
   expires: new Date(Date.now() + accessTokenExpire * 60 * 60 * 1000),
   maxAge: accessTokenExpire * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "lax",
+  sameSite: "None",
 };
 export const refreshTokenOptions = {
   expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
   maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
   httpOnly: true,
-  sameSite: "lax",
+  sameSite: "None",
 };
 
 export const sendToken = (user, statusCode, res) => {

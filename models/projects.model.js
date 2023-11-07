81fc37cf0c55621d43e9c1df03e7d920ff6b6179
ixahil <sahilshaiikh@hotmail.com>
2023-11-07 16:40:02 +0530

    adding new feature

--- models/projects.model.js
+++ models/projects.model.js
@@ -9,20 +9,21 @@ const projectSchema = new Schema(
     description: {
       type: String,
     },
-    htmlDescription: {
-      type: String,
-    },
     images: [
       {
         public_id: String,
         imageURL: String, // URL or file path to the image
       },
     ],
-    thumbnail: {
-      public_id: String,
-      imageURL: String, // URL or file path to the image
-    },
     selectedTech: [],
+    source: {
+      type: String,
+      required: [true, "Please enter Source Code URL"],
+    },
+    demo: {
+      type: String,
+      required: [true, "Please enter Demo URL"],
+    },
     status: {
       type: Boolean,
       default: false,
